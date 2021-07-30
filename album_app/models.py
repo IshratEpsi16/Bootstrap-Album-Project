93cf53ac44d1dac9195a82e410addd52be9ffffe
Your Name <ishratepsi16@gmail.comgit config --global user.name Your>
2021-07-30 13:48:38 +0600

    change

--- album_app/models.py
+++ album_app/models.py
@@ -5,6 +5,17 @@ class album_db(models.Model):
     image = models.ImageField(upload_to='album/photo/')
     description = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
+   
+
     def __str__(self):
         return self.description
+class album_db2(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='album/photo/')
+    description = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+   
+
+    def __str__(self):
+        return self.name
     
